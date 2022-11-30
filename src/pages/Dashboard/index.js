9dd2ae4739dee3746c75363bf35147efab456dfe
juliamalta <juliamalta12@gmail.com>
2022-11-30 16:39:38 -0300

    tela de chamados

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,7 +4,7 @@ import Title from "../../components/TItle";
 import Header from "../../components/Header";
 import  {FiMessageSquare, FiSave} from "react-icons/fi"
 import "./dash.css";
-import {FiPlus} from "react-icons/fi"
+import {FiPlus,FiEdit} from "react-icons/fi"
 import {FiSearch} from "react-icons/fi"
 import {FiEdit2} from "react-icons/fi"
 import New from "../New";
@@ -12,7 +12,7 @@ import { Link} from "react-router-dom";
 
 export default function Dashboard(){
   
-    const [chamados,setChamados] = useState([]);
+    const [chamados,setChamados] = useState([1]);
       return (
         <div>
             <Header/>
@@ -39,6 +39,34 @@ export default function Dashboard(){
                 <FiPlus size = {25} ></FiPlus>
                 Novo Chamado
                 </Link>
+
+
+                <table>
+<thead>
+    <tr>
+        <th scope="col">Cliente</th>
+        <th scope="col">Assunto</th>
+        <th scope="col">Status</th>
+        <th scope="col">Cadastro em</th>     
+        <th scope="col">#</th>  
+    </tr>
+</thead>
+<tbody>
+    <tr>
+        <td data-label="Cliente">Sujeito</td>
+        <td data-label="Assunto">Sujeito</td>
+        <td data-label="Status">
+            <span className="badge" style={{backgroundColor: '#5cb85c'}}>Em aberto</span>
+        </td>
+        <td data-label="Cadastrado">20/06/2021</td>
+        <td data-label="#">
+            <button className="action" style={{backgroundColor: '#3583f6'}}><FiSearch color="#FFF" size={17} ></FiSearch></button>
+            <button className="action" style={{backgroundColor: '#F6a935'}}><FiEdit color="#FFF" size={17} ></FiEdit></button>
+        </td>
+    </tr>
+</tbody>
+
+                </table>
              </>
 
           )}
