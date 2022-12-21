7384e3999427555ef391b3a49537ddbeb9d0cc1d
juliamalta <juliamalta12@gmail.com>
2022-12-21 15:15:42 -0300

    dash features

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,26 +2,82 @@ import { useContext, useState,useEffect } from "react";
 import  {AuthContext} from '../../Contexts/auth';
 import Title from "../../components/TItle";
 import Header from "../../components/Header";
-import  {FiMessageSquare, FiSave } from "react-icons/fi"
+import  {FiInstagram, FiMessageSquare, FiSave } from "react-icons/fi"
 import "./dash.css";
 import {FiPlus,FiEdit} from "react-icons/fi"
 import {FiSearch ,FiTrash} from "react-icons/fi"
 import {FiEdit2} from "react-icons/fi"
 import New from "../New";
 import { Link} from "react-router-dom";
-
-
-
+import firebase from "../../services/firebaseConnection";
+import { format } from "date-fns";
 
 export default function Dashboard(){
   
-
+    const listRef =  firebase.firestore().collection('chamados').orderBy('created','desc')
     const [chamados,setChamados] = useState([1]);
     const[loadCustomers,setLoadCustomers]= useState(true);
+    const [loadingMore,setLoadingMore]=useState(false);
+    const [isEmpty, setIsEmpty] = useState(false);
+   const [lastDocs,setLastDocs] = useState();
 
 
 
+    useEffect(()=>{
  
+    loadChamados();
+    
+    return ()=>{
+
+    }
+},[]);
+
+async function loadChamados(){
+ 
+    await listRef.limit(5)
+    .get()
+    .then((snapshot)=>{
+     
+ uptadeState(snapshot)
+
+    })
+        .catch((err)=>{
+         console.log('deu algum erro:',err);
+        setLoadingMore(false);
+    })
+setLoadCustomers(false);
+
+}
+
+
+ async function uptadeState(snapshot){
+  const isCollectionEmpty = snapshot.size === 0;
+
+if(!isCollectionEmpty){
+    let lista = [];
+    snapshot.forEach((doc)=>{
+        lista.push({
+            id : doc.id,
+            assunto : doc.data().assunto,
+            cliente : doc.data().cliente,
+            clienteId : doc.data().clienteId,
+            created : doc.data().created,
+            createdFormated : format(doc.data().created.toDate(),'dd/MM/yyyy'),
+            status : doc.data().status,
+            complemento : doc.data().complemento
+        })
+    })
+const  lastDoc = snapshot.docs[snapshot.docs.length -1];
+
+setChamados(chamados => [...chamados,...lista])
+setLastDocs(lastDoc);
+
+}else {
+    setIsEmpty(true);
+}
+setLoadingMore(false);
+
+ }
 
 
 
@@ -64,13 +120,16 @@ export default function Dashboard(){
     </tr>
 </thead>
 <tbody>
+{chamados.map((item,index)=>{
+return(
+           
     <tr>
-        <td data-label="Cliente">Sujeito</td>
-        <td data-label="Assunto">Sujeito</td>
+        <td data-label="Cliente">{item.cliente}</td>
+        <td data-label="Assunto">{item.assunto}</td>
         <td data-label="Status">
-            <span className="badge" style={{backgroundColor: '#5cb85c'}}>Em aberto</span>
+            <span className="badge" style={{backgroundColor: '#5cb85c'}}>{item.status}</span>
         </td>
-        <td data-label="Cadastrado">08/12/2022</td>
+        <td data-label="Cadastrado">{item.createdFormated}</td>
         <td data-label="#">
             <button className="action" style={{backgroundColor: '#3583f6'}}><FiSearch color="#FFF" size={17} ></FiSearch></button>
             <button className="action" style={{backgroundColor: '#F6a935'}}><FiEdit color="#FFF" size={17} ></FiEdit></button>
@@ -78,6 +137,8 @@ export default function Dashboard(){
 
         </td>
     </tr>
+    )
+})}
 </tbody>
 
                 </table>
